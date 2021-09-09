a499b44b5a389373720821691dcb00782274f553
deasisrj1 <rdeasis@ualberta.ca>
2021-09-09 14:42:28 -0600

    update

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ print('requests version number {0}'.format(requests.__version__))
 response = requests.get('http://www.google.com')
 
 print(response)
+print(response.content)
+
+response = requests.get('https://raw.githubusercontent.com/deasisrj1/404lab1/master/main.py')
+
+print(response)
+print(response.content)
