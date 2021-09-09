17eb1c1c25a859720141de9e36134fd5bbb81804
deasisrj1 <rdeasis@ualberta.ca>
2021-09-09 14:36:07 -0600

    fix main

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import requests
 print('requests version number {0}'.format(requests.__version__))
 
 response = requests.get('http://www.google.com')
-# response = requests.get('https://www.google.com')
+
 print(response)
